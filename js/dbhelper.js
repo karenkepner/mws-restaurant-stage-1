6c36e02ed3f43d1ef096e685b8f4f78a88d48097
Karen Kepner <kkepner14@gmail.com>
2019-04-17 21:38:37 -0600

    Add alt text to the restaurant images
    add key value pair for alt text to the restaurants.json data
    file for each restaurant
    
    add function altTextForRestaurant to retrieve the alt text in
    the dbhelper.js file
    
    call altTextForRestaurant function in the main.js file to insert
    the alt text in the image element

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -152,6 +152,13 @@ class DBHelper {
   static imageUrlForRestaurant(restaurant) {
     return (`/img/${restaurant.photograph}`);
   }
+  /**
+   * Restaurant image alt text
+   */
+  static altTextForRestaurant(restaurant) {
+    return (`${restaurant.alt}`);
+  }
+
 
   /**
    * Map marker for a restaurant.
