b73c035aa56d3c48e00281884220ba2a40bb6394
Karen Kepner <kkepner14@gmail.com>
2019-05-02 16:37:31 -0600

    Correct path to json file

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -10,7 +10,8 @@ class DBHelper {
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
     // return `http://localhost:${port}/data/restaurants.json`;
-    return `../data/restaurants.json`;
+    // return `/restaurant/data/restaurants.json`;
+    return `${window.location.href}/data/restaurants.json`;
   }
 
   /**
